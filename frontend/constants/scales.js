94ce4555497a6d718f1e939d2483bdafb159e3d3
App Academy Student <student@appacademy.io>
2016-07-15 16:47:33 -0700

    refactored

--- frontend/constants/scales.js
+++ frontend/constants/scales.js
@@ -2,5 +2,7 @@ module.exports = {
   Major: [0, 2, 2, 1, 2, 2, 2, 1],
   NauralMinor: [0, 2, 1, 2, 2, 1, 2, 2],
   HarmonicMinor: [0, 2, 1, 2, 2, 1, 3, 1],
-  Chromatic: [0,1,1,1,1,1,1,1,1,1,1,1,1]
+  Chromatic: [0,1,1,1,1,1,1,1,1,1,1,1,1],
+  MajPentatonic: [0,2,2,3,2,3],
+  MinPentatonic: [0,3,2,2,3,2]
 };
